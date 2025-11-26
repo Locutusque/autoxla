a245ece1335408ea4e845cd218c15e38bc72b560
Locutusque <122424773+Locutusque@users.noreply.github.com>
2025-11-25 17:57:29 -0800

    Individual model splash attention implementation

--- kernels/attention/splash_attention/models/__init__.py
+++ kernels/attention/splash_attention/models/__init__.py
@@ -1,6 +1,7 @@
 from .qwen3 import Qwen3SplashAttention
 from .gemma2 import Gemma2SplashAttention
 from .llama import LlamaSplashAttention
+from ..sa_xla import _BaseSplashAttentionWrapper
 from transformers import PreTrainedModel
 
 ATTN_CLASSES = {
@@ -10,5 +11,5 @@ ATTN_CLASSES = {
 }
 
 def _get_attention_class_from_model(model: PreTrainedModel):
-    attn_class = ATTN_CLASSES.get(model.config.model_type, LlamaSplashAttention)
+    attn_class = ATTN_CLASSES.get(model.config.model_type, _BaseSplashAttentionWrapper)
     return attn_class
